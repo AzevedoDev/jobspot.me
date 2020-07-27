fb2ebb481cf395ebd97fbcfac68d9a015998ce2e
Jonathan <johnzballad@gmail.com>
2020-07-27 08:15:31 -0300

    commit mutation function

---
+++ packages/client/src/relay/commit.ts
@@ -0,0 +1,25 @@
+import { commitMutation, GraphQLTaggedNode } from 'react-relay';
+import { PayloadError } from 'relay-runtime';
+import Environment from './Environment';
+
+function commit(
+  mutation: GraphQLTaggedNode,
+  data: Record<string, unknown>,
+  onCompleted: (
+    response: Record<string, unknown>,
+    errors: readonly PayloadError[],
+  ) => void,
+  onError: (error: Error) => void,
+) {
+  const variables = {
+    data,
+  };
+
+  return commitMutation(Environment, {
+    mutation,
+    variables,
+    onCompleted,
+    onError,
+  });
+}
+export default { commit };
